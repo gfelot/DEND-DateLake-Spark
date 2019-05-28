fea8da8b41ed72589adcd00f3a837ec9793d338f
Gil <gfelot@gmail.com>
2019-05-28 12:30:02 +0200

    Fix log_path error

--- etl.py
+++ etl.py
@@ -84,7 +84,7 @@ def process_log_data(spark, input_data, output_data):
 
    """
     # get filepath to log data file
-    log_path = input_data + 'log_data/*.json'
+    log_path = input_data + 'log_data/*/*/*.json'
 
     # read log data file
     df = spark.read.json(log_path)
